99eae9ade758c609e671c2a64d1191203c68a533
Andrey Ulyanenko <ua.ulin@gmail.com>
2014-12-27 22:38:10 +0200

    Update fb.js

--- fb.js
+++ fb.js
@@ -40,7 +40,7 @@ window.fbAsyncInit = function() {
    		var html="";
    		for (var i=0, l=res.data.length;i<l;i++) {
    			var it = res.data[i], capt="";
-   			if(it.status_type == "shared_story" $$ it.caption.length>140) capt = "<p class = 'caption'>"+it.caption+"</p>"
+   			if(it.status_type && it.status_type == "shared_story" && it.caption && it.caption.length>140) capt = "<p class = 'caption'>"+it.caption+"</p>"
    			if(res.data[i].message||capt) html += "<div class = 'post "+res.data[i].type+"'><p>"
    			+res.data[i].from.name+" ("+(res.data[i].type).slice(0,1)+") — "+goodDate(res.data[i].updated_time)
    			+"</p>"+res.data[i].message+capt+"</div>"
